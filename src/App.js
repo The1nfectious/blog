973e7e84bd8eb1dfea0bea9d8756a5279410c8a1
Ali Çağlar Alhan <alhan.alicaglar@gmail.com>
2023-12-22 20:25:08 +0300

    This is my first react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Ali Çağlar Alhan @CagsLhn",
+      message: "This is my first react project",
+      timestamp: new Date(),
+    },
+    {
+      username: "Lewis Hamilton @LH44",
+      message: "Great Team with Amazing Fans",
+      timestamp: new Date(),
+    },
+    {
+      username: "Galatasaray @galatasaray",
+      message: "IIIIICAAAARRRRDIIIII",
+      timestamp: new Date(),
+    },
+    {
+      username: "Ali Çağlar Alhan @CagsLhn",
+      message: "Fullstack Developer is my goal and one day i will be...",
+      timestamp: new Date(),
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <h1>Have a nice days:) </h1>
+      <div className="posts">
+        {posts.map((post, index) => (
+          <Post
+            key={index}
+            username={post.username}
+            message={post.message}
+            timestamp={post.timestamp}
+          />
+        ))}
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+        <p style={{textAlign: "center"}}>That's all for today from what you follow</p>        
+    </footer>
+  );
+}
+
+export default Footer;
